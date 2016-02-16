f6a9ea0612f00e0760e4dc1e2779e7b0f8435ea2
Colin Hunt <colin.hunt@live.com>
2016-02-16 13:34:53 -0700

    Got computed values to work on focus out.

--- app/components/sheet-cell/component.js
+++ app/components/sheet-cell/component.js
@@ -6,7 +6,9 @@ export default Ember.Component.extend({
 	value: Ember.computed('grid', {
 		get() {
 			console.log(`getting... ${this.get('grid').get(this.get('cid'))}`);
-			return this.get('grid').get(this.get('cid')) || "";
+			// return the computed value ?
+			const value = this.get('grid').get(this.get('cid'));
+			return value || "";
 		},
 		set(key, value) {
 			console.log(`setting it to ${value}`);
@@ -14,10 +16,26 @@ export default Ember.Component.extend({
 			return value;
 		}
 	}),
+	computedValue: Ember.computed('value', function() {
+		return this.computeValue();
+	}),
+
+	computeValue() {
+		console.log("getting computed value...2");
+		const value = this.get('value');
+		try {
+			if (value.charAt(0) === "=") {
+				return eval(value.substring(1));
+			}
+		} catch(e) {
+			return "NaN";
+		}
+		return value;
+	},
 
 	focusIn(e) {
 		console.log("focusin... on component " + this.get('cid'));
-		// e.target.value = this.get('value');
+		e.target.value = this.get('value');
 	},
 
 	focusOut(e) {
@@ -25,5 +43,6 @@ export default Ember.Component.extend({
 		// e.target.value = e.target.value.toLowerCase();
 		console.log("focusout...");
 		this.set('value', e.target.value);
-	}
+		e.target.value = this.computeValue();
+	},
 });
